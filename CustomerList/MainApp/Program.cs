61407a066b73c41154d1878a86818c08aa5f5488
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2025-09-04 14:06:32 +0200

    2025-09-04

--- CustomerList/MainApp/Program.cs
+++ CustomerList/MainApp/Program.cs
@@ -1,24 +1,28 @@
 ﻿using MainApp.Models;
+using MainApp.Services;
 
-var customer = new Customer();
-customer.CustomerName = "Avanoria AB";
+var customerService = new CustomerService();
 
-customer.ContactInformation = new ContactInformation();
-customer.ContactInformation.Email = "kundservice@avanoria.se";
-customer.ContactInformation.PhoneNumber = "+46 8 121 470 50";
 
-customer.ContactPerson = new ContactPerson();
-customer.ContactPerson.FirstName = "Hans";
-customer.ContactPerson.LastName = "Mattin-Lassei";
-customer.ContactPerson.Email = "hans.mattin-lassei@avanoria.se";
 
-customer.BillingAddress = new Address();
-customer.BillingAddress.AddressLine_1 = "Nordkapsvägen 1";
-customer.BillingAddress.PostalCode = "136 57";
-customer.BillingAddress.City = "Vega";
+var customer_1 = new Customer();
+customer_1.CustomerName = "  Avanoria AB";
 
-customer.DeliveryAddress = new Address();
-customer.DeliveryAddress.AddressLine_1 = "Nordkapsvägen 1";
-customer.DeliveryAddress.AddressLine_2 = "Godsmottagning Port A";
-customer.DeliveryAddress.PostalCode = "136 57";
-customer.DeliveryAddress.City = "Vega";
+var customer_2 = new Customer();
+customer_2.CustomerName = "Nackademin AB  ";
+
+var customer_3 = new Customer();
+customer_3.CustomerName = "  Tietoevry";
+
+customerService.Create(customer_1);
+customerService.Create(customer_2);
+customerService.Create(customer_3);
+
+
+var customers = customerService.GetAll();
+
+foreach (var customer in customers)
+    Console.WriteLine(customer.CustomerName);
+
+
+Console.ReadKey();
--- CustomerList/MainApp/Services/CustomerService.cs
+++ CustomerList/MainApp/Services/CustomerService.cs
@@ -1,9 +1,19 @@
-﻿namespace MainApp.Services;
+﻿using MainApp.Models;
+
+namespace MainApp.Services;
 
 public class CustomerService
 {
-    // fields       camelCase       firstName
+    private List<Customer> _customerList = [];
 
-    // Methods      PascalCase      GetFirstName()
+    public void Create(Customer customer)
+    {   
+        customer.CustomerName = customer.CustomerName.Trim();
+        _customerList.Add(customer);
+    }
 
+    public IEnumerable<Customer> GetAll()
+    {
+        return _customerList;
+    }
 }
