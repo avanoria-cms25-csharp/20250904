f7b6ab79694f31191fe23e0b55b4756b6e9db249
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2025-09-04 15:01:42 +0200

    2025-09-04

--- CustomerList/MainApp/Program.cs
+++ CustomerList/MainApp/Program.cs
@@ -1,28 +1,5 @@
-﻿using MainApp.Models;
-using MainApp.Services;
+﻿using MainApp.Services;
 
-var customerService = new CustomerService();
+var menuService = new MenuService();
 
-
-
-var customer_1 = new Customer();
-customer_1.CustomerName = "  Avanoria AB";
-
-var customer_2 = new Customer();
-customer_2.CustomerName = "Nackademin AB  ";
-
-var customer_3 = new Customer();
-customer_3.CustomerName = "  Tietoevry";
-
-customerService.Create(customer_1);
-customerService.Create(customer_2);
-customerService.Create(customer_3);
-
-
-var customers = customerService.GetAll();
-
-foreach (var customer in customers)
-    Console.WriteLine(customer.CustomerName);
-
-
-Console.ReadKey();
+menuService.DisplayMainMenu();
---
+++ CustomerList/MainApp/Services/MenuService.cs
@@ -0,0 +1,81 @@
+﻿using MainApp.Models;
+
+namespace MainApp.Services;
+
+public class MenuService
+{
+    private CustomerService _customerService = new CustomerService();
+
+
+    public void DisplayMainMenu()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("### MENU OPTIONS ###");
+            Console.WriteLine("1. Create Customer");
+            Console.WriteLine("2. View Customer List");
+
+            Console.WriteLine();
+            Console.Write("Choose a menu option: ");
+            var option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    DisplayAddCustomerOption();
+                    break;
+
+                case "2":
+                    DisplayListCustomersOption();
+                    break;
+            }
+        }
+    }
+
+
+    private void DisplayAddCustomerOption()
+    {
+        Console.Clear();
+        Console.WriteLine("### NEW CUSTOMER ###");
+
+        var customer = new Customer();
+
+        while (string.IsNullOrEmpty(customer.CustomerName))
+        {
+            Console.Write("Enter Customer Name: ");
+            customer.CustomerName = Console.ReadLine()!;
+
+            if (string.IsNullOrEmpty(customer.CustomerName))
+            {
+                Console.WriteLine("You must enter a valid customer name.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        _customerService.Create(customer);
+
+        Console.WriteLine();
+        Console.WriteLine("Customer was created successfully.");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+
+    private void DisplayListCustomersOption()
+    {
+        Console.Clear();
+        Console.WriteLine("### CUSTOMER LIST ###");
+
+        var customerList = _customerService.GetAll();
+
+        foreach (var customer in customerList)
+        {
+            Console.WriteLine(customer.CustomerName);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+}
